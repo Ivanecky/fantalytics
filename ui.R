1009515511f01bc2e492f4c19d687d04bd113539
Ivanecky <sam.ivanecky3@gmail.com>
2020-11-22 13:03:47 -0600

    Transitioned to a navbar setting

--- ui.R
+++ ui.R
@@ -7,68 +7,42 @@ library(plotly)
 library(DT)
 
 shinyUI(# Define page
-    fluidPage(
-        
-        # Title
-        titlePanel(
-            "Fantasy Football EDA"
+    navbarPage("ESPN FF Analysis",
+               # Player Analysis
+        tabPanel("Player Search",
+            fluidPage(
+                # Title
+                titlePanel(
+                    "Fantasy Football EDA"
+                ),
+                # Sidebar
+                sidebarPanel(
+                    # Widgets for searching
+                    # Name
+                    textInput("player_name", h3("Player Name"), value = "Aaron Rodgers"),
+                    # Search button
+                    actionButton("player_search", label = "Search")
+                ),
+                # Main
+                mainPanel(
+                    fluidRow(
+                        DT::dataTableOutput('player_performances'),
+                    ),
+                    fluidRow(
+                        plotlyOutput("player_perf_plot")
+                    ),
+                    fluidRow(
+                        plotlyOutput("player_weekly_plot")
+                    )
+                )
+                
+            )
         ),
-        
-        # Sidebar
-        sidebarPanel(
-            # Widgets for searching
-            # Name
-            textInput("player_name", h3("Player Name"), value = "Aaron Rodgers"),
-            # Search button
-            actionButton("player_search", label = "Search")
-        ),
-        
-        # Main
-        mainPanel(
-            fluidRow(
-                DT::dataTableOutput('player_performances'),
-            ),
-            fluidRow(
-                plotlyOutput("player_perf_plot")
-            ),
-            fluidRow(
-                plotlyOutput("player_weekly_plot")
+        # Position Analysis
+        tabPanel("Position Analysis",
+            fluidPage(
+                
             )
         )
-        
     )
-    # dashboardPage(
-    #     # Define page header
-    #     dashboardHeader(title = "ESPN FF"),
-    #     # Define sidebar
-    #     dashboardSidebar(
-    #         # Sidebar Menu
-    #         sidebarMenu(
-    #             menuItem("Main", tabName = "main"),
-    #             menuItem("+/- Performances", tabName = "pt_performances"),
-    #             menuItem("About", tabName = "about")
-    #         )
-    #     ),
-    #     # Define body
-    #     dashboardBody(
-    #         tabItems(
-    #             tabItem(
-    #                 "main",
-    #                 # Top summary row
-    #                 fluidRow(
-    #                     DT::dataTableOutput("playerPerformances")
-    #                 )
-    #             ),
-    #             tabItem(
-    #                 "pt_performances",
-    #                 fluidRow(
-    #                     DT::dataTableOutput("overperformers")
-    #                 ),
-    #                 fluidRow(
-    #                     DT::dataTableOutput("underperformers")
-    #                 )
-    #             )
-    #         )
-    #     )
-    # )
 )
